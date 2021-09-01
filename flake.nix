613c0ec6622550759712377419b1066727f31825
Sven Friedrich <friedrich122112@googlemail.com>
2021-09-01 13:42:17 +0200

    CI

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system: {
+    flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ] (system: {
       packages = { libusb = inputs.libusb.packages.${system}.libusb; };
     });
 }
