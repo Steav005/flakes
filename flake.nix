1836028d706684eddfc8a0097a0e0f9124ee304c
Autumnal <friedrich122112@me.com>
2021-11-20 22:39:30 +0100

    fcitx5-nord

--- flake.nix
+++ flake.nix
@@ -4,10 +4,15 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     libusb.url = "path:libusb";
+    fcitx5-nord.url = "path:fcitx5-nord";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ] (system: {
-      packages = { libusb = inputs.libusb.packages.${system}.libusb; };
+    flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
+    (system: {
+      packages = {
+        libusb = inputs.libusb.packages.${system}.libusb;
+        fcitx5-nord = inputs.fcitx5-nord.packages.${system}.fcitx5-nord;
+      };
     });
 }
--- libusb/flake.nix
+++ libusb/flake.nix
@@ -4,7 +4,8 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ] (system:
+    flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
+    (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
         packages = flake-utils.lib.flattenTree {
